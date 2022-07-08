b0e07eef4f182030abdc8c6c7c826463531b0f55
VitMur <vovammee@gmail.com>
2022-07-08 14:06:40 +0300

    основа V1

--- Lineage2/Lineage2/apps/Info/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from .models import Info
-from django.http import HttpResponse
-# Create your views here.
-
-
-def index(request):
-    return render(request, 'Info/Info.html',)
